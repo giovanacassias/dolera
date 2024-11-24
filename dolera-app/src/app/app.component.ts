10bd9cd213f59080b97d43fd3b1e0c925e597eb1
Giovana Cassias <giovanacassias@outlook.com>
2024-11-23 21:13:12 -0300

    projeto angular iniciado - problema de versões corrigido

--- dolera-app/src/app/app.component.ts
+++ dolera-app/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OlaMundoComponent } from "./ola-mundo/ola-mundo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OlaMundoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dolera-app';
+  title = 'Dolera';
 }
