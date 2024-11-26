74fc79c33e54772a6fe6ef5abb72921b7b90ab2c
Giovana Cassias <giovanacassias@outlook.com>
2024-11-25 21:58:30 -0300

    segundo componente criado

---
+++ dolera-app/src/app/welcome-dolera/welcome-dolera.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-dolera',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome-dolera.component.html',
+  styleUrl: './welcome-dolera.component.css'
+})
+export class WelcomeDoleraComponent {
+
+}
